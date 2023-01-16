e8e70a260ccccaaf5d6cfdc8e86e17d4a3f43b8b
eyndjl <eyndjl@eyndjl.debian>
2023-01-16 12:47:30 +0500

    См. changelog.txt

--- config_eybie.py
+++ config_eybie.py
@@ -14,18 +14,21 @@ splashes = [
 "Also try Terraria", 
 "Появился значит в зоне...", 
 "Спасите",
-"Сделано на Python 3!"
+"Сделано на Python 3!",
+"S.O.S. МЕНЯ ДЕРЖАТ В ЗА...",
+"в теаргИ"
 ]
 
 #Настройки
 settings = {
 	'token': 'TOKEN',
-	'bot_name': 'Eybie' #Имя Eybie
+	'bot_name': 'Eybie', #Имя Eybie
+	'avatar_path':'avatar.png' #Путь до желаемой аватарки [png, jpg]
 }
 
 #Информация о текущей версии бота
 version_info = {
-	'version': '3.0.5',
+	'version': '3.0.6',
 	'rel_date': '16.01.2023',
 	'codename': 'Passion Fruit',
 	'distribution': 'Main'
