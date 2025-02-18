54776aa14a97d50c058e2b93643c46dfdde2470a
Viktor Sapi <viksapi2k@gmail.com>
2025-02-18 18:37:24 +0500

    Minor changes

--- modules/vi_commands.py
+++ modules/vi_commands.py
@@ -1,20 +1,18 @@
-from modules.eybie import *
+from modules.viksa import *
 
 logging.info("Загружен модуль EY_COMMANDS")
 
 @bot.command(name="info", description="Основная информация (Время работы, разработчики, версия бота)")
 async def info(ctx):
     cursession = str(datetime.timedelta(seconds=int(round(time.time()-startTime))))
-    embed = discord.Embed(title="Eybie", description=f"Текущая версия: {eybie_ver}", colour=0xFFE933)
-    embed.add_field(name="Прочее", value=f"Дистрибутив: {eybie_distro}, статус профайлера {PROFSTATE}")
+    embed = discord.Embed(title="Viksa", description=f"Текущая версия: {viksa_ver}", colour=0xFFE933)
+    embed.add_field(name="Прочее", value=f"Дистрибутив: {viksa_distro}, статус профайлера {PROFSTATE}")
     embed.add_field(name="Время работы", value=f"{cursession}", inline=False)
-    embed.add_field(name="Разработчик", value="Eyndjl#2356", inline=True)
-    embed.add_field(name="Оф.сайт", value="https://eyndjl.github.io", inline=True)
     await ctx.response.send_message(embed=embed, ephemeral=True)
     logging.info(f"Использование команды: /info пользователем {ctx.author.name} с id: {ctx.author.id}")
 
-@bot.command(name="github", description="Вывод Github'a Eybie")
+@bot.command(name="github", description="Вывод Github'a Viksa")
 async def github(ctx):
     logging.info(f"Использование команды: /github пользователем {ctx.author.name} с id: {ctx.author.id} | Спасибки <3")
-    embed = discord.Embed(title='Github', description="Страница Eybie в Github: https://github.com/Eyndjl/eybie", colour=0xFFE933)
+    embed = discord.Embed(title='Github', description="Страница Viksa в Github: https://github.com/viksapi2k/viksa", colour=0xFFE933)
     await ctx.response.send_message(embed=embed)
