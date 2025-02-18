c92353ddd4db19c644d2d66a70f858987b5939d6
Viktor Sapi <viksapi2k@gmail.com>
2025-02-18 20:20:12 +0500

    Translating into English..

--- run.py
+++ run.py
@@ -5,11 +5,11 @@ from modules.vi_commands import *
 from modules.warn_system import *
 
 if settings['token'] == "TOKEN" or "":
-    logging.error("Ууупс... вы забыли добавить токен для Eybie. Получить токен можно тут: https://discord.com/developers/applications, токен вписывается в config_eybie.py")
+    logging.error("Oops... You forgot to add a token for Viksa. You can get it here: https://discord.com/developers/applications, token fits into config_viksa.py")
 else:
-    logging.info("Токен подходит, попытка запуска...")
+    logging.info("Token is valid. Trying to start...")
     bot.run(settings['token'])
-    logging.info("Попытка успешна. Приятного использования Eybie!")
+    logging.info("Bot is running.")
 
 if KeyboardInterrupt:
-    logging.info("Выключение...")
+    logging.info("Turning off...")
