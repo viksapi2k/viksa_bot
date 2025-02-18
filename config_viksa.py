f27a4bae427b9ca69d27c63a4303c59bc06f580f
Viktor Sapi <viksapi2k@gmail.com>
2025-02-18 18:34:39 +0500

    Minor changes

---
+++ config_viksa.py
@@ -0,0 +1,22 @@
+#Splashes
+splashes = [
+"Only in Discord!", 
+"Also try Minecraft",
+"Also try DingoLingo", 
+"Also try Terraria"
+]
+
+#Settings
+settings = {
+	'token': 'TOKEN',
+	'bot_name': 'Viksa', #Имя Eybie
+	'avatar_path':'avatar.png' #Путь до желаемой аватарки [png, jpg]
+}
+
+#Version info
+version_info = {
+	'version': '1.0.0',
+	'rel_date': '18.02.2025',
+	'codename': 'Not set',
+	'distribution': 'Main'
+}
