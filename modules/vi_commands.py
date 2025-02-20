56dac06663728fba4c12a1fa79bc9aa91ec2df2d
Viktor Sapi <viksapi2k@gmail.com>
2025-02-20 09:16:29 +0500

    Translating into English..

--- modules/vi_commands.py
+++ modules/vi_commands.py
@@ -2,7 +2,7 @@ from modules.viksa import *
 
 logging.info("Module loaded VI_COMMANDS")
 
-@bot.command(name="info", description="Основная информация (Время работы, разработчики, версия бота)")
+@bot.command(name="info", description="Main information (Uptme, developers, bot version)")
 async def info(ctx):
     cursession = str(datetime.timedelta(seconds=int(round(time.time()-startTime))))
     embed = discord.Embed(title="Viksa", description="Current version: {viksa_ver}", colour=0xFFE933)
@@ -11,7 +11,7 @@ async def info(ctx):
     await ctx.response.send_message(embed=embed, ephemeral=True)
     logging.info(f"Command usage: /info by {ctx.author.name} with id: {ctx.author.id}")
 
-@bot.command(name="github", description="Вывод Github'a Viksa")
+@bot.command(name="github", description="Viksa's Github")
 async def github(ctx):
     logging.info(f"Command usage: /github by {ctx.author.name} with id: {ctx.author.id} | Thanks <3")
     embed = discord.Embed(title='Github', description="Viksa's Github-page: https://github.com/viksapi2k/viksa", colour=0xFFE933)
