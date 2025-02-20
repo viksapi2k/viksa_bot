c5355818c0bfe7c4ba354d41759e2b583ba994e3
Viktor Sapi <viksapi2k@gmail.com>
2025-02-20 09:18:05 +0500

    Translating into English..

--- modules/vi_msg.py
+++ modules/vi_msg.py
@@ -1,14 +1,14 @@
 from modules.viksa import *
 
-logging.info("Загружен модуль VI_MSG")
+logging.info("Module loaded VI_MSG")
 
-@bot.command(name="say", description="Вывод сообщения от имени бота")
+@bot.command(name="say", description="Send a message on behalf of the bot")
 @has_permissions(administrator=True)
 async def say(ctx, text):
     await ctx.send(text)
     embed = discord.Embed(title=message_sent, description=" ", colour=0xFFE933)
     await ctx.response.send_message(embed=embed, ephemeral=True)
-    logging.info(f"Использование команды: /say пользователем {ctx.author.name} с id: {ctx.author.id} | Текст сообщения: \"{text}\"") 
+    logging.info(f"Command usage: /say by {ctx.author.name} with id: {ctx.author.id} | Текст сообщения: \"{text}\"") 
 
 @say.error
 async def say_error(ctx, error):
