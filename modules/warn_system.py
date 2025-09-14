06a59a6f23adca75a8ce4243527f4e810f3ff2ff
Viktor Sapi <viksapi2k@gmail.com>
2025-09-14 11:51:58 +0500

    wrong dir lol

--- modules/warn_system.py
+++ modules/warn_system.py
@@ -1,23 +1,23 @@
 from modules.viksa import *
 import sqlite3
 
-logging.info("Загружен модуль WARN_SYSTEM")
+logging.info("Module loaded WARN_SYSTEM")
 
-#Настройки
-WARNSCOUNT = 3 #Максимальное число предупреждений, при достижении указанного числа выдаётся блокировка
-BLOCKDURATION = 10080 #Длительность блокировки, указывается в минутах
+#Settings
+WARNSCOUNT = 3 #Max warns. >3 = timeout
+BLOCKDURATION = 10080 #Timeout duration
 
-#Создание необходимых папок
+#Default folders
 if not os.path.isdir(f"database/servers"):
-    logging.info("Папка \"database/servers\" не найдена. Создание папки...")
+    logging.info("There is no \"database/servers\". Creating...")
     os.mkdir(f'database')
     os.mkdir(f'database/servers')
 
-#Система варнов на sqlite3.
-@bot.command(name="warn", description="Выдать предупреждение пользователю")
+#Warn system. Based on sqlite3.
+@bot.command(name="warn", description="Warn user")
 @has_permissions(administrator=True)
 async def warn(ctx, member: discord.Member, reason: str):
-    logging.info(f"Использование команды: /warn пользователем {ctx.author.name} с id: {ctx.author.id} | Причина: \"{reason}\"") 
+    logging.info(f"Command usage: /warn by {ctx.author.name} with id: {ctx.author.id} | Reason: \"{reason}\"") 
     serverid = ctx.guild.id
     if not os.path.isdir(f"database/servers/{serverid}"):
         os.mkdir(f'database/servers/{serverid}')
@@ -30,11 +30,11 @@ async def warn(ctx, member: discord.Member, reason: str):
     cursor.execute(f"INSERT INTO {userid} (reason) VALUES('{reason_full}');")
     sqlite_conn.commit()
     warns = len(cursor.execute(f"SELECT * FROM {userid}").fetchall())
-    embed = discord.Embed(title=f'Пользователю {member.name} выдано предупреждение.', description=f'Причина: {reason}', colour=0xFFE933)
+    embed = discord.Embed(title=f'User {member.name} was warned.', description=f'Reason: {reason}', colour=0xFFE933)
     await ctx.response.send_message(embed=embed)
     if warns >= WARNSCOUNT:
-        logging.info(f"Пользователь {member.name} заблокирован (warns >= {WARNSCOUNT})")
-        embed2 = discord.Embed(title="Пользователю был ограничен доступ к текстовому и голосовому чату на 7 дней за нарушения правил.", description='', colour=0xFFE933)
+        logging.info(f"User {member.name} was temporary blocked (warns >= {WARNSCOUNT})")
+        embed2 = discord.Embed(title="The user's access to text and voice chat was restricted for 7 days for violating the rules.", description='', colour=0xFFE933)
         await member.timeout_for(datetime.timedelta(minutes=BLOCKDURATION))
         await ctx.send(embed=embed2)
         cursor.execute(f"DELETE FROM {userid}")
@@ -48,20 +48,20 @@ async def warn_error(ctx, error):
     embed = discord.Embed(title=error_text, description=" ", colour=0xFFE933)
     await ctx.response.send_message(embed=embed, ephemeral=True)
 
-@bot.command(name="warns", description="Отобразить список предупреждений пользователя")
+@bot.command(name="warns", description="Show user's warn list")
 @has_permissions(administrator=True)
 async def warns(ctx, user:discord.User):
-    logging.info(f"Использование команды: /warns пользователем {ctx.author.name} с id: {ctx.author.id}")
+    logging.info(f"Command usage: /warns by {ctx.author.name} with id: {ctx.author.id}")
     serverid = ctx.guild.id
     sqlite_conn = sqlite3.connect(f'database/servers/{serverid}/users.db')
     cursor = sqlite_conn.cursor()
     userid = "id" + str(user.id)
     warns_reasons = cursor.execute(f"SELECT reason FROM {userid}").fetchall()
     warns = len(warns_reasons)
-    reasons_clean = str(warns_reasons).replace("[(\'", '').replace("(\'", '').replace("\',)", '').replace(",", '\n') #Добавить проверку на количество варнов
+    reasons_clean = str(warns_reasons).replace("[(\'", '').replace("(\'", '').replace("\',)", '').replace(",", '\n')
     reasons_form = str(reasons_clean[:len(reasons_clean)-1])
-    embed = discord.Embed(title=f'Предупреждения пользователя {user.name}', description='  ', colour=0xFFE933)
-    embed.add_field(name=f"Количество предупреждений: {warns}, причины:", value=f"{reasons_form}", inline=False)
+    embed = discord.Embed(title=f'Warnings of {user.name}', description='  ', colour=0xFFE933)
+    embed.add_field(name=f"Warnings count: {warns}, reasons:", value=f"{reasons_form}", inline=False)
     await ctx.response.send_message(embed=embed, ephemeral=True)
 
 @warns.error
